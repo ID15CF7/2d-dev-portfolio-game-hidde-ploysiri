d2e2f5401329b5dd11bd2f3732ad67cd27718b29
Hidde <51237260+ID15CF7@users.noreply.github.com>
2024-04-15 21:25:36 +0200

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: 'Dit is mijn pc opzet. Hier werk ik aan projecten om mijn codeer vaardigheden te verbeteren. Ik richt me vooral op Javascript. Als frontend developer wil ik mezelf verbreden door ook de backend te leren. Vandaar dat ik mijn skills nu verder uitbreid met NodeJS en NextJS. Je kunt de code van deze game bekijken op mijn <a href="https://github.com/ID15CF7/2d-dev-portfolio-game-hidde-ploysiri" target="_blank">Github pagina</a>. Ik werk met de volgende code stack: HTML/CSS (Tailwind CSS en Bootstrap), Javascript (REACT, Node en Next), PHP, Python.',
+    library: 'In mijn boekenkast vind je vooral boeken over psychologie, online marketing, natuurkunde, programmeren, ondernemerschap, financieen, filosofie en biografieen van grote tech ondernemers. Ik ben altijd op zoek naar nieuwe inzichten en manieren om mezelf te ontwikkelen. Mijn favoriete boeken zijn: "Antifragiel" van Nassim Nicholas Taleb, "De antwoorden op grote vragen" van Stephen Hawking, "Dictatuur van het geluk" van Jeffrey Wijnberg en "The map of knowledge" van Violet Moller.',
+    sofa: 'Op doordeweekse avonden en vroeg in de ochtend in het weekend zit ik graag op de bank om mijn boeken te lezen. Af en toe kijk ik dan met mijn vriendin op de bank naar een film. Verder kijk ik nooit tv.',
+    sidetable: 'Op mijn bijzettafels vind je altijd een of meerdere boeken, een notitieboek met pen en een glas thee.',
+    bed: 'Mijn bed is echt een plek om tot rust te komen. Ook daar lees ik graag nog wat of schrijf ik kort in mijn notitieboek om de dag rustig af te sluiten. Ik lig het liefste tussen 21:00 - 22:00 uur in bed en ik sta dan om 5 uur op.',
+    light: 'Ook op het nachtkasje naast mijn bed vind je naast een lamp ook altijd enkele boeken.',
+    table: 'Op deze tafel zie je mijn cv liggen met een QR code naar mijn LinkedIn. Je kunt ook op deze link klikken voor <a href="https://www.linkedin.com/in/hidde-ploysiri/" target="_blank">mijn LinkedIn-pagina</a>. En je kunt hier <a href="" target="_blank">mijn CV bekijken</a>. Wil je direct contact met me opnemen? Stuur gerust een mail naar <a href="mailto:hidde@ploysiri.nl">hidde@ploysiri.nl</a>, een whatsapp naar <a href="https://wa.me/31610874881" target="_blank">+31610874881</a> of een bericht via <a href="https://www.linkedin.com/in/hidde-ploysiri/" target="_blank">LinkedIn</a>.',
+    exit: 'Wil je het spel verlaten? Sluit dan de tab in je browser of je hele browser.',
+    tree: 'Bomen en de natuur zijn belangrijk voor mij. Ik wandel graag samen met mijn vriendin en onze 2 adoptiehonden in het bos of op het strand. Naast pianospelen kom ik ook erg tot rust door in de natuur te zijn.',
+}
