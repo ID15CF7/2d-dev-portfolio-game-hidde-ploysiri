9eba348dc7e331386b7e202acdb9cb861338eecb
Hidde <51237260+ID15CF7@users.noreply.github.com>
2024-04-16 08:24:55 +0200

    URL aanpassing Vite App

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./",
+    base: "/2d-dev-portfolio-game-hidde-ploysiri/",
     build: {
         minify: "terser",
     },
